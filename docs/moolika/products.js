9e7352028bb37adef94c5955cf8c9810cf46b9d3
Naredula Janardhana Reddy <naredula.jana@gmail.com>
2026-04-11 14:23:04 +0530

    Create products.js

---
+++ docs/moolika/products.js
@@ -0,0 +1,16 @@
+var allProducts = [
+
+{ name:"Rice", price:60, category:"grains", organic:true, diabetic:false, sizes:[5,20] },
+{ name:"Wheat", price:50, category:"grains", organic:false, diabetic:false, sizes:[5,10] },
+{ name:"Jowar", price:60, category:"grains", organic:true, diabetic:true, sizes:[5,10] },
+
+{ name:"Ragi", price:70, category:"millets", organic:true, diabetic:true, sizes:[5] },
+{ name:"Foxtail", price:90, category:"millets", organic:true, diabetic:true, sizes:[5] },
+
+{ name:"Groundnut Oil", price:180, category:"oils", organic:true, diabetic:true, sizes:[1] },
+{ name:"Coconut Oil", price:200, category:"oils", organic:true, diabetic:true, sizes:[1] },
+
+{ name:"Jaggery", price:60, category:"misc", organic:true, diabetic:true, sizes:[1] },
+{ name:"Honey", price:300, category:"misc", organic:true, diabetic:true, sizes:[1] }
+
+];
