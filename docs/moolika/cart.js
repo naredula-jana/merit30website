4e1aa55ec36ca0aa40ba0a0b0c3b1008db81f50e
Naredula Janardhana Reddy <naredula.jana@gmail.com>
2026-04-12 20:32:45 +0530

    Update cart.js

--- docs/moolika/cart.js
+++ docs/moolika/cart.js
@@ -1,3 +1,4 @@
+// GET CART
 function getCart(){
   try{
     return JSON.parse(localStorage.getItem("cart")) || {};
@@ -6,18 +7,19 @@ function getCart(){
   }
 }
 
+// SAVE CART
 function saveCart(cart){
   localStorage.setItem("cart", JSON.stringify(cart));
   renderCart();
 }
 
-function addItem(name,price){
+// ADD ITEM
+function addItem(name, price){
 
   var cart = getCart();
 
   var size = document.getElementById("size_"+name).value;
-
-  var key = name+"_"+size;
+  var key = name + "_" + size;
 
   if(!cart[key]){
     cart[key] = {
@@ -33,6 +35,33 @@ function addItem(name,price){
   saveCart(cart);
 }
 
+// CHANGE QUANTITY (+ / -)
+function changeQty(key, delta){
+
+  var cart = getCart();
+
+  if(cart[key]){
+    cart[key].qty += delta;
+
+    if(cart[key].qty <= 0){
+      delete cart[key];
+    }
+  }
+
+  saveCart(cart);
+}
+
+// REMOVE ITEM
+function removeItem(key){
+
+  var cart = getCart();
+
+  delete cart[key];
+
+  saveCart(cart);
+}
+
+// RENDER CART (FINAL UI)
 function renderCart(){
 
   var cart = getCart();
@@ -44,20 +73,40 @@ function renderCart(){
 
     var i = cart[k];
 
-    var price = Number(i.price) || 0;
-    var size  = Number(i.size) || 0;
-    var qty   = Number(i.qty) || 0;
+    var size = String(i.size).replace("kg","");
+    var price = Number(i.price);
+    var qty   = Number(i.qty);
 
-    var t = price * size * qty;
+    var t = price * Number(size) * qty;
 
     total += t;
 
-    html += i.name + " (" + size + "kg) x " + qty + " = ₹" + t + "<br>";
+    html += `
+    <div style="display:flex;justify-content:space-between;align-items:center;margin-bottom:8px;border-bottom:1px solid #ddd;padding-bottom:5px;">
+
+      <div style="flex:2;">
+        <b>${i.name}</b><br>
+        <small>${size}kg</small>
+      </div>
+
+      <div style="flex:2;text-align:center;">
+        <button onclick="changeQty('${k}', -1)">➖</button>
+        ${qty}
+        <button onclick="changeQty('${k}', 1)">➕</button>
+      </div>
+
+      <div style="flex:1;text-align:right;">
+        ₹${t}
+      </div>
+
+      <div style="flex:0.5;text-align:right;">
+        <button onclick="removeItem('${k}')">❌</button>
+      </div>
+
+    </div>
+    `;
   }
 
-  var cartDiv = document.getElementById("cart");
-  var totalDiv = document.getElementById("total");
-
-  if(cartDiv) cartDiv.innerHTML = html || "Empty";
-  if(totalDiv) totalDiv.innerHTML = total;
+  document.getElementById("cart").innerHTML = html || "Empty";
+  document.getElementById("total").innerHTML = total;
 }
