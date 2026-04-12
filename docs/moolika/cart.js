e1ed39bf9662cef81abdc579d2b8e48f872c7493
Naredula Janardhana Reddy <naredula.jana@gmail.com>
2026-04-12 21:00:32 +0530

    Update cart.js

--- docs/moolika/cart.js
+++ docs/moolika/cart.js
@@ -1,3 +1,70 @@
+// ===== GET CART =====
+function getCart(){
+  try{
+    return JSON.parse(localStorage.getItem("cart")) || {};
+  }catch(e){
+    return {};
+  }
+}
+
+// ===== SAVE CART =====
+function saveCart(cart){
+  localStorage.setItem("cart", JSON.stringify(cart));
+  renderCart();
+}
+
+// ===== ADD ITEM =====
+function addItem(name, price){
+
+  var cart = getCart();
+
+  var sizeEl = document.getElementById("size_" + name);
+  if(!sizeEl) return;
+
+  var size = parseFloat(sizeEl.value) || 0;
+  var key = name + "_" + size;
+
+  if(!cart[key]){
+    cart[key] = {
+      name: name,
+      size: size,
+      price: parseFloat(price),
+      qty: 0
+    };
+  }
+
+  cart[key].qty += 1;
+
+  saveCart(cart);
+}
+
+// ===== CHANGE QUANTITY =====
+function changeQty(key, delta){
+
+  var cart = getCart();
+
+  if(cart[key]){
+    cart[key].qty += delta;
+
+    if(cart[key].qty <= 0){
+      delete cart[key];
+    }
+  }
+
+  saveCart(cart);
+}
+
+// ===== REMOVE ITEM =====
+function removeItem(key){
+
+  var cart = getCart();
+
+  delete cart[key];
+
+  saveCart(cart);
+}
+
+// ===== RENDER CART (SAFE VERSION) =====
 function renderCart(){
 
   var cart = getCart();
@@ -9,35 +76,38 @@ function renderCart(){
 
     var i = cart[k];
 
-    var size = String(i.size).replace("kg","");
-    var price = Number(i.price);
-    var qty   = Number(i.qty);
+    var size = parseFloat(i.size) || 0;
+    var price = parseFloat(i.price) || 0;
+    var qty = parseFloat(i.qty) || 0;
 
-    var t = price * Number(size) * qty;
+    var t = price * size * qty;
     total += t;
 
-    html += '<div style="border-bottom:1px solid #eee;padding:6px 0;font-size:13px;">'
+    html += "<div style='padding:6px;border-bottom:1px solid #eee;'>";
 
-          + '<div><b>' + i.name + ' (' + size + 'kg)</b></div>'
+    html += "<div><b>" + i.name + " (" + size + "kg)</b></div>";
 
-          + '<div style="display:flex;justify-content:space-between;align-items:center;margin-top:4px;">'
+    html += "<div style='display:flex;justify-content:space-between;margin-top:5px;'>";
 
-          + '<div>'
-          + '<button onclick="changeQty(\'' + k + '\', -1)">➖</button> '
-          + qty +
-          ' <button onclick="changeQty(\'' + k + '\', 1)">➕</button>'
-          + '</div>'
+    html += "<div>";
+    html += "<button onclick=\"changeQty('" + k + "', -1)\">-</button> ";
+    html += qty;
+    html += " <button onclick=\"changeQty('" + k + "', 1)\">+</button>";
+    html += "</div>";
 
-          + '<div>₹' + t + '</div>'
+    html += "<div>₹" + t + "</div>";
 
-          + '<div>'
-          + '<button onclick="removeItem(\'' + k + '\')">❌</button>'
-          + '</div>'
+    html += "<div>";
+    html += "<button onclick=\"removeItem('" + k + "')\">X</button>";
+    html += "</div>";
 
-          + '</div>'
-          + '</div>';
+    html += "</div>";
+    html += "</div>";
   }
 
-  document.getElementById("cart").innerHTML = html || "Empty";
-  document.getElementById("total").innerHTML = total;
+  var cartDiv = document.getElementById("cart");
+  var totalDiv = document.getElementById("total");
+
+  if(cartDiv) cartDiv.innerHTML = html || "Empty";
+  if(totalDiv) totalDiv.innerHTML = total;
 }
