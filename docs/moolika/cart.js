e9d9ac868985018755286b4bd5b1507e557a720c
Naredula Janardhana Reddy <naredula.jana@gmail.com>
2026-04-12 21:33:22 +0530

    Update cart.js

--- docs/moolika/cart.js
+++ docs/moolika/cart.js
@@ -1,113 +1,74 @@
-// ===== GET CART =====
 function getCart(){
-  try{
-    return JSON.parse(localStorage.getItem("cart")) || {};
-  }catch(e){
-    return {};
-  }
+try{return JSON.parse(localStorage.getItem("cart"))||{};}catch(e){return {};}
 }
 
-// ===== SAVE CART =====
 function saveCart(cart){
-  localStorage.setItem("cart", JSON.stringify(cart));
-  renderCart();
+localStorage.setItem("cart",JSON.stringify(cart));
+renderCart();
 }
 
-// ===== ADD ITEM =====
-function addItem(name, price){
+function addItem(name,price){
 
-  var cart = getCart();
+var cart=getCart();
+var size=parseFloat(document.getElementById("size_"+name).value)||0;
+var key=name+"_"+size;
 
-  var sizeEl = document.getElementById("size_" + name);
-  if(!sizeEl) return;
-
-  var size = parseFloat(sizeEl.value) || 0;
-  var key = name + "_" + size;
-
-  if(!cart[key]){
-    cart[key] = {
-      name: name,
-      size: size,
-      price: parseFloat(price),
-      qty: 0
-    };
-  }
-
-  cart[key].qty += 1;
-
-  saveCart(cart);
+if(!cart[key]){
+cart[key]={name:name,size:size,price:price,qty:0};
 }
 
-// ===== CHANGE QUANTITY =====
-function changeQty(key, delta){
-
-  var cart = getCart();
-
-  if(cart[key]){
-    cart[key].qty += delta;
-
-    if(cart[key].qty <= 0){
-      delete cart[key];
-    }
-  }
-
-  saveCart(cart);
+cart[key].qty++;
+saveCart(cart);
 }
 
-// ===== REMOVE ITEM =====
+function changeQty(key,delta){
+
+var cart=getCart();
+
+if(cart[key]){
+cart[key].qty+=delta;
+if(cart[key].qty<=0){delete cart[key];}
+}
+
+saveCart(cart);
+}
+
 function removeItem(key){
-
-  var cart = getCart();
-
-  delete cart[key];
-
-  saveCart(cart);
+var cart=getCart();
+delete cart[key];
+saveCart(cart);
 }
 
-// ===== RENDER CART (SAFE VERSION) =====
 function renderCart(){
 
-  var cart = getCart();
+var cart=getCart();
+var html="";
+var total=0;
 
-  var html = "";
-  var total = 0;
+for(var k in cart){
 
-  for(var k in cart){
+var i=cart[k];
+var t=i.price*i.size*i.qty;
+total+=t;
 
-    var i = cart[k];
+html+="<div style='padding:6px;border-bottom:1px solid #eee;'>";
 
-    var size = parseFloat(i.size) || 0;
-    var price = parseFloat(i.price) || 0;
-    var qty = parseFloat(i.qty) || 0;
+html+="<div><b>"+i.name+" ("+i.size+"kg)</b></div>";
 
-    var t = price * size * qty;
-    total += t;
+html+="<div style='display:flex;justify-content:space-between;'>";
 
-    html += "<div style='padding:6px;border-bottom:1px solid #eee;'>";
+html+="<div>";
+html+="<button onclick=\"changeQty('"+k+"',-1)\">-</button> "+i.qty+" ";
+html+="<button onclick=\"changeQty('"+k+"',1)\">+</button>";
+html+="</div>";
 
-    html += "<div><b>" + i.name + " (" + size + "kg)</b></div>";
+html+="<div>₹"+t+"</div>";
 
-    html += "<div style='display:flex;justify-content:space-between;margin-top:5px;'>";
+html+="<div><button onclick=\"removeItem('"+k+"')\">X</button></div>";
 
-    html += "<div>";
-    html += "<button onclick=\"changeQty('" + k + "', -1)\">-</button> ";
-    html += qty;
-    html += " <button onclick=\"changeQty('" + k + "', 1)\">+</button>";
-    html += "</div>";
+html+="</div></div>";
+}
 
-    html += "<div>₹" + t + "</div>";
-
-    html += "<div>";
-    html += "<button onclick=\"removeItem('" + k + "')\">X</button>";
-    html += "</div>";
-
-    html += "</div>";
-    html += "</div>";
-  }
-
-  var cartDiv = document.getElementById("cart");
-  var totalDiv = document.getElementById("total");
-
-  if(cartDiv) cartDiv.innerHTML = html || "Empty";
-  if(totalDiv) totalDiv.innerHTML = total;
+document.getElementById("cart").innerHTML=html||"Empty";
+document.getElementById("total").innerHTML=total;
 }
