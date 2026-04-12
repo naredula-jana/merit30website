df893998a48682a88e66e77898b2b337dcc55c8d
Naredula Janardhana Reddy <naredula.jana@gmail.com>
2026-04-12 17:49:04 +0530

    Update products.js

--- docs/moolika/products.js
+++ docs/moolika/products.js
@@ -1,27 +1,20 @@
 var allProducts = [
 
-{ name:"Rice", price:60, category:"grains", organic:true, diabetic:false, sizes:[5,20] },
-{ name:"Wheat", price:50, category:"grains", organic:false, diabetic:false, sizes:[5,10] },
-{ name:"Jowar", price:60, category:"grains", organic:true, diabetic:true, sizes:[5,10] },
-  // PULSES (added under grains)
+{ name:"Rice", price:60, category:"grains", sizes:[5,20] },
+{ name:"Wheat", price:50, category:"grains", sizes:[5,10] },
+{ name:"Jowar", price:60, category:"grains", sizes:[5,10] },
 
-{ name:"Toor Dal", price:120, category:"grains", organic:true, diabetic:true, sizes:[1,5] },
-{ name:"Moong Dal", price:110, category:"grains", organic:true, diabetic:true, sizes:[1,5] },
-{ name:"Chana Dal", price:90, category:"grains", organic:true, diabetic:true, sizes:[1,5] },
-{ name:"Urad Dal", price:130, category:"grains", organic:true, diabetic:true, sizes:[1,5] },
+{ name:"Toor Dal", price:120, category:"grains", sizes:[1,5] },
+{ name:"Moong Dal", price:110, category:"grains", sizes:[1,5] },
+{ name:"Chana Dal", price:90, category:"grains", sizes:[1,5] },
 
-{ name:"Green Gram", price:100, category:"grains", organic:true, diabetic:true, sizes:[1,5] },
-{ name:"Black Chana", price:80, category:"grains", organic:true, diabetic:true, sizes:[1,5] },
+{ name:"Ragi", price:70, category:"millets", sizes:[5] },
+{ name:"Foxtail", price:90, category:"millets", sizes:[5] },
 
-  
-{ name:"Ragi", price:70, category:"millets", organic:true, diabetic:true, sizes:[5] },
-{ name:"Foxtail", price:90, category:"millets", organic:true, diabetic:true, sizes:[5] },
+{ name:"Groundnut Oil", price:180, category:"oils", sizes:[1] },
+{ name:"Coconut Oil", price:200, category:"oils", sizes:[1] },
 
-{ name:"Groundnut Oil", price:180, category:"oils", organic:true, diabetic:true, sizes:[1] },
-{ name:"Coconut Oil", price:200, category:"oils", organic:true, diabetic:true, sizes:[1] },
-
-
-{ name:"Jaggery", price:60, category:"misc", organic:true, diabetic:true, sizes:[1] },
-{ name:"Honey", price:300, category:"misc", organic:true, diabetic:true, sizes:[1] }
+{ name:"Jaggery", price:60, category:"misc", sizes:[1] },
+{ name:"Honey", price:300, category:"misc", sizes:[1] }
 
 ];
