9987df0648096dec00e81c7afec2352f0f40d40b
Naredula Janardhana Reddy <naredula.jana@gmail.com>
2026-04-12 21:34:59 +0530

    Update products.js

--- docs/moolika/products.js
+++ docs/moolika/products.js
@@ -1,12 +1,10 @@
-var allProducts = [
+var allProducts=[
 
 { name:"Rice", price:60, category:"grains", sizes:[5,20] },
 { name:"Wheat", price:50, category:"grains", sizes:[5,10] },
-{ name:"Jowar", price:60, category:"grains", sizes:[5,10] },
 
 { name:"Toor Dal", price:120, category:"grains", sizes:[1,5] },
 { name:"Moong Dal", price:110, category:"grains", sizes:[1,5] },
-{ name:"Chana Dal", price:90, category:"grains", sizes:[1,5] },
 
 { name:"Ragi", price:70, category:"millets", sizes:[5] },
 { name:"Foxtail", price:90, category:"millets", sizes:[5] },
